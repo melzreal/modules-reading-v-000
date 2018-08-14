9628097563bb9ab2171ef9ae962226c728daa304
Melissa Real <melissa.real@hotmail.com>
2018-08-14 12:54:41 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,6 +3,9 @@ require_relative './fancy_dance.rb'
 class Dancer
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
+  include FancyDance::InstanceMethods
+  
+  
   attr_accessor :name 
   
    def initialize(name)
